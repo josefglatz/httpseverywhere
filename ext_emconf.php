d847a866aac3837b2d63784003004ed6d8513189
Josef Glatz <j.glatz@supseven.at>
2023-09-06 13:04:17 +0200

    [RELEASE] Raise version number to 1.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '11.5.0-12.4.99',
         ],
     ],
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'state' => 'stable',
     'autoload' => [
         'psr-4' => [
