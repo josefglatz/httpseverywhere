b1792872aa395b8a94bf3697a8bfc9db275c8aab
Josef Glatz <typo3@josefglatz.at>
2024-10-26 07:48:59 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Middleware for forcing HTTPS',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
         ],
     ],
     'version' => '1.0.4',
